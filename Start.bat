2785fb54c3c0ddeec9bf675dae98ee9f41054839
Antadapra <134060116+Antadapra@users.noreply.github.com>
2026-03-18 20:11:53 +0300

    Add files via upload

---
+++ Start.bat
@@ -0,0 +1,3 @@
+.\Server\RustDedicated.exe -batchmode -nographics +server.dataDirectory "D:/RustServer/Server" +server.hostname "server_name" +server.port 28015 +server.identity "server_name" +server.seed 1899866745 +server.worldsize 3000 -logFile "output.txt" +rcon.port 28016 +rcon.password "654465" +server.maxplayers 69 +server.level "Procedural Map"
+
+pause
---
+++ Update.bat
@@ -0,0 +1,2 @@
+D:\RustServer\steamCMD/steamcmd.exe +login anonymous +force_install_dir D:\RustServer\Server\ +app_update 258550 validate +quit
+pause

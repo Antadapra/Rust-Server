4a1ae03786edb84d1ba45deae9b57b14a502fdae
Antadapra <134060116+Antadapra@users.noreply.github.com>
2026-03-18 20:18:06 +0300

    Delete Start.bat

--- Start.bat
+++
@@ -1,3 +0,0 @@
-.\Server\RustDedicated.exe -batchmode -nographics +server.dataDirectory "D:/RustServer/Server" +server.hostname "server_name" +server.port 28015 +server.identity "server_name" +server.seed 1899866745 +server.worldsize 3000 -logFile "output.txt" +rcon.port 28016 +rcon.password "654465" +server.maxplayers 69 +server.level "Procedural Map"
-
-pause
